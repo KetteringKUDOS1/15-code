44a4462a93a9df3b5d1d6fffa626adb883b50644
dawayne8926w <110747359+dawayne8926w@users.noreply.github.com>
2024-04-21 19:35:08 -0400

    update

--- src/PTO_motors.cpp
+++ src/PTO_motors.cpp
@@ -1,7 +1,7 @@
 #include "pros/motors.h"
 #include "pros/motors.hpp"
+#include "PTO_motors.hpp"
 
 
-
-pros::Motor left_PTO_motor(18, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor right_PTO_motor(19, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor left_PTO_motor(18, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor right_PTO_motor(19, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
